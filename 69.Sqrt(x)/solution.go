665c86e0ab9e50e50ad2710e8cfe7298788bdd80
huirenjie <hui.renjie@whaley.cn>
2019-09-02 13:24:56 +0800

    add 69

--- 69.Sqrt(x)/solution.go
+++ 69.Sqrt(x)/solution.go
@@ -9,19 +9,24 @@ import (
 
 
 解题思路：
-
+牛顿法
 
 关键点：
 
 
 */
 
-func mySqrt(x int) int { // faster 100% less 100%.
-
+func mySqrt(x int) int { // faster 100% less 83.33%.
+	res := x
+	// 牛顿法求平方根
+	for res*res > x {
+		res = (res + x/res) / 2
+	}
+	return res
 }
 
 func main() {
-	x := 4
+	x := 188
 	result := mySqrt(x)
 	fmt.Println(result)
 }
---
+++ 70.Climbing Stairs/solution.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+要求：
+
+
+解题思路：
+牛顿法
+
+关键点：
+
+
+*/
+
+func climbStairs(n int) int {// faster 100% less 83.33%.
+
+}
+
+func main() {
+	n := 188
+	result := mySqrt(x)
+	fmt.Println(result)
+}
