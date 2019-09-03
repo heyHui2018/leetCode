8ce8eb46dff79f6bb8361a110a96e61df56db4e8
huirenjie <hui.renjie@whaley.cn>
2019-09-03 13:40:19 +0800

    add 70

--- 70.Climbing Stairs/solution.go
+++ 70.Climbing Stairs/solution.go
@@ -9,19 +9,28 @@ import (
 
 
 解题思路：
-牛顿法
+f(n)=f(n-1)+f(n-2),f(0)=1,f(1)=1
 
 关键点：
 
 
 */
 
-func climbStairs(n int) int {// faster 100% less 83.33%.
-
+func climbStairs(n int) int { // faster 100% less 22.22%
+	if n < 2 {
+		return 1
+	}
+	rec := make([]int, n+1)
+	rec[0], rec[1] = 1, 1
+	// 用slice的遍历来代替函数的递归
+	for i := 2; i <= n; i++ {
+		rec[i] = rec[i-1] + rec[i-2]
+	}
+	return rec[n]
 }
 
 func main() {
-	n := 188
-	result := mySqrt(x)
+	n := 45
+	result := climbStairs(n)
 	fmt.Println(result)
 }
---
+++ 71.Simplify Path/solution.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+要求：
+
+
+解题思路：
+
+
+关键点：
+
+
+*/
+
+func simplifyPath(path string) string { // faster 100% less 22.22%
+
+}
+
+func main() {
+	path := "123123"
+	result := simplifyPath(n)
+	fmt.Println(result)
+}
