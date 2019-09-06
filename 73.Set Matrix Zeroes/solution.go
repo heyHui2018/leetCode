a66697cd3d6dd0a46a5e74dfda3126c5bc34a999
e <962691478@qq.com>
2019-09-06 11:10:16 +0800

    add 73

--- 73.Set Matrix Zeroes/solution.go
+++ 73.Set Matrix Zeroes/solution.go
@@ -6,22 +6,43 @@ import (
 
 /*
 要求：
-
+Do it in-place
 
 解题思路：
-
+遍历矩阵,当发现某行出现0时,在遍历完该行后将此行全置为0,同时将出现0的列存入map,最后遍历map将对应的列置为0
 
 关键点：
 
 
 */
 
-func setZeroes(matrix [][]int) { // faster 77.99% less 100%
-
+func setZeroes(matrix [][]int) { // faster 85.78% less 100%
+	columnMap := map[int]int{}
+	for i := range matrix {
+		flag := false
+		for j := range matrix[i] {
+			if matrix[i][j] == 0 {
+				flag = true
+				columnMap[j] = 1
+			}
+		}
+		if flag {
+			for k := range matrix[i] {
+				matrix[i][k] = 0
+			}
+		}
+	}
+	for m := range columnMap {
+		for i := range matrix {
+			matrix[i][m] = 0
+		}
+	}
 }
 
 func main() {
-	matrix := [][]int{{1}, {1}}
+	matrix := [][]int{{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}}
 	setZeroes(matrix)
-	fmt.Println(matrix)
+	for k := range matrix {
+		fmt.Println(matrix[k])
+	}
 }
---
+++ 74.Search a 2D Matrix/solution.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+要求：
+
+
+解题思路：
+
+
+关键点：
+
+
+*/
+
+func searchMatrix(matrix [][]int, target int) bool { // faster 85.78% less 100%
+
+}
+
+func main() {
+	matrix := [][]int{{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}}
+	target := 4
+	result := searchMatrix(matrix, target)
+	fmt.Println(result)
+}
