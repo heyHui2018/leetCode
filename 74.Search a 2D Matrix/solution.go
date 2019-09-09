9ed09a068f6fa4bda09f02c69ff08d84ecba4ea6
e <962691478@qq.com>
2019-09-09 14:36:46 +0800

    add 74

--- 74.Search a 2D Matrix/solution.go
+++ 74.Search a 2D Matrix/solution.go
@@ -9,20 +9,52 @@ import (
 
 
 解题思路：
-
+先根据每行的首位确定目标所要比较的行,随后二分法判断是否存在
 
 关键点：
 
 
 */
 
-func searchMatrix(matrix [][]int, target int) bool { // faster 85.78% less 100%
-
+func searchMatrix(matrix [][]int, target int) bool { // faster 96.98% less 100%
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return false
+	}
+	l := len(matrix[0])
+	c := -1
+	for k := range matrix {
+		if matrix[k][l-1] >= target && target >= matrix[k][0] {
+			c = k
+			break
+		}
+	}
+	if c == -1 {
+		return false
+	}
+	left := 0
+	right := l - 1
+	for {
+		if left == right || left == right-1 {
+			if matrix[c][left] == target || matrix[c][right] == target {
+				return true
+			}
+			break
+		}
+		switch {
+		case matrix[c][(left+right)/2] > target:
+			right -= (right - left) / 2
+		case matrix[c][(left+right)/2] < target:
+			left += (right - left) / 2
+		default:
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
-	matrix := [][]int{{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}}
-	target := 4
+	matrix := [][]int{{-9, -7, -7, -5, -3}, {-1, 0, 1, 3, 3}, {5, 7, 9, 11, 12}, {13, 14, 15, 16, 18}, {19, 21, 22, 22, 22}}
+	target := -4
 	result := searchMatrix(matrix, target)
 	fmt.Println(result)
 }
---
+++ 75.Sort Colors/solution.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+要求：
+
+
+解题思路：
+
+
+关键点：
+
+
+*/
+
+func sortColors(nums []int) { // faster 96.98% less 100%
+
+}
+
+func main() {
+	nums := []int{5, 7, 9, 11, 12}
+	sortColors(nums)
+	fmt.Println(nums)
+}
