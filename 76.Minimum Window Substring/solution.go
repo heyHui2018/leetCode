45e519b4026cde44ddce90c5389f8f94ebc3f6ef
e <962691478@qq.com>
2019-09-11 14:09:30 +0800

    add 76

--- 76.Minimum Window Substring/solution.go
+++ 76.Minimum Window Substring/solution.go
@@ -9,7 +9,8 @@ import (
 in complexity O(n)
 
 解题思路：
-
+统计t中出现的字符及数量,遍历s,当发现需要的字符时,定为窗口的左侧,继续遍历直到窗口包括了t中的所有字符,
+此时开始收缩左侧窗口直到刚好包括t中所有字符,记为一次满足记录，与已有记录比较,更小时覆盖记录,最终返回最小的记录
 
 关键点：
 
@@ -17,7 +18,37 @@ in complexity O(n)
 */
 
 func minWindow(s string, t string) string { // faster 100% less 100%
+	have := [128]int{}
+	need := [128]int{}
+	// 存放ASCII码0-128的数组,need[t[i]]++可继续字符的出现次数
+	for i := range t {
+		need[t[i]]++
+	}
+	size := len(s)
+	total := len(t)
+	min := size + 1
+	res := ""
 
+	for i, j, count := 0, 0, 0; j < size; j++ {
+		if have[s[j]] < need[s[j]] {
+			// 出现需要的字符
+			count++
+		}
+		have[s[j]]++
+
+		for i <= j && have[s[i]] > need[s[i]] {
+			// 缩小窗口
+			have[s[i]]--
+			i++
+		}
+
+		width := j - i + 1
+		if count == total && min > width {
+			min = width
+			res = s[i : j+1]
+		}
+	}
+	return res
 }
 
 func main() {
---
+++ 77.Combinations/solution.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+要求：
+in complexity O(n)
+
+解题思路：
+
+
+关键点：
+
+
+*/
+
+func combine(n int, k int) [][]int { // faster 100% less 100%
+
+}
+
+func main() {
+	n := 3
+	k := "ABC"
+	result := combine(n, k)
+	fmt.Println(result)
+}
