18b8e2c3bee29e37935895b98eb9a0a9d3e07ca8
e <962691478@qq.com>
2019-09-12 14:15:55 +0800

    add 77

--- 77.Combinations/solution.go
+++ 77.Combinations/solution.go
@@ -6,23 +6,42 @@ import (
 
 /*
 要求：
-in complexity O(n)
+
 
 解题思路：
-
+回溯算法,当某一次形成长度=k的组合后,回溯到之前k-1的长度并继续迭代形成新的组合,当此次k-1的迭代完成后,回溯到长度=k-2继续迭代
 
 关键点：
 
 
 */
 
-func combine(n int, k int) [][]int { // faster 100% less 100%
+func combine(n int, k int) [][]int { // faster 65.64% less 100%
+	var res [][]int
+	c(&res, []int{}, 1, n, k)
+	return res
+}
 
+func c(res *[][]int, coms []int, start int, n int, k int) {
+	if k == 0 {
+		tmp := []int{}
+		tmp = append(tmp, coms...)
+		*res = append(*res, tmp)
+		return
+	}
+	for i := start; i <= n; i++ {
+		coms = append(coms, i)
+		fmt.Println("1111111111 coms = ", coms)
+		c(res, coms, i+1, n, k-1)
+		// 回溯
+		coms = coms[:len(coms)-1]
+		fmt.Println("22222 coms = ", coms)
+	}
 }
 
 func main() {
-	n := 3
-	k := "ABC"
+	n := 4
+	k := 2
 	result := combine(n, k)
 	fmt.Println(result)
 }
---
+++ 78.Subsets/solution.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+要求：
+
+
+解题思路：
+
+
+关键点：
+
+
+*/
+
+func subsets(nums []int) [][]int { // faster 65.64% less 100%
+
+}
+
+func main() {
+	nums := []int{1, 2, 3}
+	result := subsets(nums)
+	fmt.Println(result)
+}
