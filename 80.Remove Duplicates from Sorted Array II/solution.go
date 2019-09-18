ff409da0b5d313330d5e2f218505262bd1387728
e <962691478@qq.com>
2019-09-18 11:42:01 +0800

    add 80

--- 80.Remove Duplicates from Sorted Array II/solution.go
+++ 80.Remove Duplicates from Sorted Array II/solution.go
@@ -9,19 +9,26 @@ import (
 in-place
 
 解题思路：
-
+取i表示处理完成的数的下标,取j=i往后遍历,当nums[i-2]!=nums[j]即发现一个新的数时,将i位置的数与j位置的数交换并将i+1,此时j的值也+1,继续遍历
 
 关键点：
 
 
 */
 
-func removeDuplicates(nums []int) int { // faster 100% less 50%
-
+func removeDuplicates(nums []int) int { // faster 93.30% less 100%
+	i := 2
+	for j := i; j < len(nums); j++ {
+		if nums[i-2] != nums[j] {
+			nums[i] = nums[j]
+			i++
+		}
+	}
+	return i
 }
 
 func main() {
-	nums := []int{1, 2, 3}
+	nums := []int{1, 2, 2, 2, 2, 3}
 	result := removeDuplicates(nums)
 	fmt.Println(result)
 }
---
+++ 81.Search in Rotated Sorted Array II/solution.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+要求：
+in-place
+
+解题思路：
+
+
+关键点：
+
+
+*/
+
+func search(nums []int, target int) bool { // faster 93.30% less 100%
+
+}
+
+func main() {
+	nums := []int{2, 5, 6, 0, 0, 1, 2}
+	target := 0
+	result := search(nums)
+	fmt.Println(result)
+}
