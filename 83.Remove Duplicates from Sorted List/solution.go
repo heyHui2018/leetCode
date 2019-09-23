26ef4b5eb8e5ab2c7ab268fdd3fd95efcaab33f0
e <962691478@qq.com>
2019-09-23 13:45:43 +0800

    add 83

--- 83.Remove Duplicates from Sorted List/solution.go
+++ 83.Remove Duplicates from Sorted List/solution.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 /*
@@ -9,7 +10,8 @@ import (
 
 
 解题思路：
-
+遍历链表,取tempVal记录临时val,tempNode记录当前最后一个有效节点,当发现当前val=tempVal时,若当前节点next==nil,则将tempNode的next置为nil以使其成为最后一个节点,若!=nil,则head = head.Next继续遍历;
+当val!=tempVal时,更新tempVal,将当前节点链接上tempNode,并更新tempNode,后续处理同上
 
 关键点：
 
@@ -21,8 +23,33 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func deleteDuplicates(head *ListNode) *ListNode { // faster 100% less 33.33%
-
+func deleteDuplicates(head *ListNode) *ListNode { // faster 89.32% less 100%
+	if head == nil || head.Next == nil {
+		return head
+	}
+	res := head
+	tempVal := head.Val
+	tempNode := head
+	head = head.Next
+	for head != nil {
+		if head.Val == tempVal {
+			if head.Next == nil {
+				tempNode.Next = nil
+				return res
+			}
+			head = head.Next
+			continue
+		}
+		tempVal = head.Val
+		tempNode.Next = head
+		tempNode = head
+		if head.Next == nil {
+			tempNode.Next = nil
+			return res
+		}
+		head = head.Next
+	}
+	return res
 }
 
 func main() {
@@ -34,24 +61,29 @@ func main() {
 	l3 := new(ListNode)
 	l3.Val = 2
 	l2.Next = l3
-	l4 := new(ListNode)
-	l4.Val = 3
-	l3.Next = l4
-	l5 := new(ListNode)
-	l5.Val = 3
-	l4.Next = l5
-	l6 := new(ListNode)
-	l6.Val = 4
-	l5.Next = l6
-	l7 := new(ListNode)
-	l7.Val = 4
-	l6.Next = l7
-	l8 := new(ListNode)
-	l8.Val = 5
-	l7.Next = l8
+	// l4 := new(ListNode)
+	// l4.Val = 3
+	// l3.Next = l4
+	// l5 := new(ListNode)
+	// l5.Val = 3
+	// l4.Next = l5
+	// l6 := new(ListNode)
+	// l6.Val = 4
+	// l5.Next = l6
+	// l7 := new(ListNode)
+	// l7.Val = 4
+	// l6.Next = l7
+	// l8 := new(ListNode)
+	// l8.Val = 5
+	// l7.Next = l8
+	// l9 := new(ListNode)
+	// l9.Val = 5
+	// l8.Next = l9
 	result := deleteDuplicates(l1)
 	for result.Next != nil {
-		fmt.Println(result.Val)
+		time.Sleep(1 * time.Second)
+		fmt.Println(result)
 		result = result.Next
 	}
+	fmt.Println(result)
 }
---
+++ 84.Largest Rectangle in Histogram/solution.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+要求：
+
+
+解题思路：
+
+
+关键点：
+
+
+*/
+
+func largestRectangleArea(heights []int) int { // faster 89.32% less 100%
+
+}
+
+func main() {
+	heights := []int{1, 2, 3}
+	result := largestRectangleArea(heights)
+	fmt.Println(result)
+}
