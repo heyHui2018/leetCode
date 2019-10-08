c2d60bad6a47850ecc3319dba11e94bb36090964
e <962691478@qq.com>
2019-10-08 14:10:32 +0800

    add 88

--- 88.Merge Sorted Array/solution.go
+++ 88.Merge Sorted Array/solution.go
@@ -9,49 +9,43 @@ import (
 
 
 解题思路：
-递归.遍历判断两个字符是否存在某一次分割,能使s1的某一子串与s2的某一子串互为搅拌字符,s1的另一子串与s2的另一子串互为搅拌字符,一旦存在则返回true,否则返回false
-三维动态规划.
+从后往前处理可避免频繁交换.遍历nums2,与nums1中的数从后往前比较,若nums2中的数较大,则将其放在nums1的未排序的最后并继续遍历,若nums1中的数较大,则将其放在nums1的未排序的最后,
+并取nums1中的前一个数继续与nums2中的数比较直到nums2中的数被放到nums1的未排序的最后才开始下一轮遍历
 
 关键点：
 
 
 */
 
-func isScramble(s1 string, s2 string) bool { // faster 100% less 100%
-	// 相同即为互为搅拌字符
-	if s1 == s2 {
-		return true
-	}
-	// 校验包含字符是否一致
-	rec := make([]int, 256)
-	for i := 0; i < len(s1); i++ {
-		rec[s1[i]]++
-		rec[s2[i]]--
-	}
-	for i := range rec {
-		if rec[i] != 0 {
-			return false
+func merge(nums1 []int, m int, nums2 []int, n int) { // faster 100% less 66.67%
+	// p-nums1中待比较的位置,q-nums1中从后往前第一个未排序的位置
+	p := m - 1
+	q := len(nums1) - 1
+	for i := n - 1; i >= 0; i-- {
+		if p >= 0 {
+			if nums2[i] >= nums1[p] {
+				nums1[q] = nums2[i]
+			} else {
+				for p >= 0 && nums2[i] < nums1[p] {
+					nums1[q] = nums1[p]
+					nums1[p] = 0
+					q--
+					p--
+				}
+				nums1[q] = nums2[i]
+			}
+		} else {
+			nums1[q] = nums2[i]
 		}
+		q--
 	}
-	// 校验字符是否互为搅拌字符
-	for i := 1; i < len(s1); i++ {
-		// 当存在某一次分割使s1的左子串与s2的左子串互为搅拌字符,且s1的右子串与s2的右子串互为搅拌字符,true
-		if isScramble(s1[:i], s2[:i]) && isScramble(s1[i:], s2[i:]) {
-			return true
-		}
-		// 当存在某一次分割使s1的左子串与s2的右子串互为搅拌字符,且s1的右子串与s2的左子串互为搅拌字符,true
-		// s1[:i] i个字符, s2[len(s1)-i:] i个字符
-		fmt.Println(s1[:i], s2[len(s1)-i:])
-		if isScramble(s1[:i], s2[len(s1)-i:]) && isScramble(s1[i:], s2[:len(s1)-i]) {
-			return true
-		}
-	}
-	return false
 }
 
 func main() {
-	s1 := "abcde"
-	s2 := "caebd"
-	result := isScramble(s1, s2)
-	fmt.Println(result)
+	num1 := []int{2, 0}
+	m := 1
+	num2 := []int{1}
+	n := 1
+	merge(num1, m, num2, n)
+	fmt.Println(num1)
 }
---
+++ 89.Gray Code/solution.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+要求：
+
+
+解题思路：
+
+
+关键点：
+
+
+*/
+
+func merge(nums1 []int, m int, nums2 []int, n int) { // faster 100% less 66.67%
+
+
+}
+
+func main() {
+	num1 := []int{2, 0}
+	m := 1
+	num2 := []int{1}
+	n := 1
+	merge(num1, m, num2, n)
+	fmt.Println(num1)
+}
