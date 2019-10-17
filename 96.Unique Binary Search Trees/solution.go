226c12c28fb8eae3268f754b3e4156baa69ec7f0
e <962691478@qq.com>
2019-10-17 14:18:09 +0800

    add 96

--- 96.Unique Binary Search Trees/solution.go
+++ 96.Unique Binary Search Trees/solution.go
@@ -9,21 +9,24 @@ import (
 
 
 解题思路：
-
+n=0,res=1;n=1,res=1;n=2,res=2;n=3;res=5;res(n)=res(0)*res(n-1)+res(1)*res(n-2)+...+res(n-1)*res(0)卡特兰数
 
 关键点：
 
 
 */
 
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
-
-func numTrees(n int) int { // faster 61.04% less 100%
-
+func numTrees(n int) int { // faster 100% less 100%
+	list := make([]int, n+1)
+	list[0] = 1
+	list[1] = 1
+	for i := 2; i <= n; i++ {
+		for j := 0; j < i; j++ {
+			list[i] += list[j] * list[i-j-1]
+		}
+	}
+	fmt.Println(list)
+	return list[n]
 }
 
 func main() {
---
+++ 97.Interleaving String/solution.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+要求：
+
+
+解题思路：
+
+
+关键点：
+
+
+*/
+
+func isInterleave(s1 string, s2 string, s3 string) bool { // faster 100% less 100%
+
+}
+
+func main() {
+	s1 := ""
+	s2 := ""
+	s3 := ""
+	result := isInterleave(s1, s2, s3)
+	fmt.Println(result)
+}
