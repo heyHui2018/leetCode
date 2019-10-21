51780a99a500988c0cbc5086b991bf2c863c1b67
e <962691478@qq.com>
2019-10-21 11:47:25 +0800

    add 98

--- 98.Validate Binary Search Tree/solution.go
+++ 98.Validate Binary Search Tree/solution.go
@@ -9,10 +9,10 @@ import (
 
 
 解题思路：
-
+递归,同时传入当前根节点应属于的大小范围,左子树及其子树应小于根节点,右子树同理,递归时不断更新大小范围
 
 关键点：
-
+递归时传入当前根节点应属于的大小范围
 
 */
 
@@ -23,11 +23,27 @@ type TreeNode struct {
 }
 
 func isValidBST(root *TreeNode) bool { // faster 100% less 50%
-
+	MIN, MAX := -1<<63, 1<<63-1
+	return r(MIN, MAX, root)
 }
 
+func r(min, max int, root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return min < root.Val && root.Val < max && r(min, root.Val, root.Left) && r(root.Val, max, root.Right)
+}
 func main() {
 	root := new(TreeNode)
+	root.Val = 10
+	root.Left = new(TreeNode)
+	root.Right = new(TreeNode)
+	root.Left.Val = 5
+	root.Right.Val = 15
+	root.Right.Left = new(TreeNode)
+	root.Right.Right = new(TreeNode)
+	root.Right.Left.Val = 6
+	root.Right.Right.Val = 20
 	result := isValidBST(root)
 	fmt.Println(result)
 }
