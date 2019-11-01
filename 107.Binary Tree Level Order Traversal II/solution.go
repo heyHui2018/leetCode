4cbe37bce6a6edf87da63f34317e9175090c69e0
e <962691478@qq.com>
2019-11-01 11:39:43 +0800

    add 107

--- 107.Binary Tree Level Order Traversal II/solution.go
+++ 107.Binary Tree Level Order Traversal II/solution.go
@@ -9,7 +9,7 @@ import (
 
 
 解题思路：
-
+在102的基础上将其结果的第一维倒叙遍历即可
 
 关键点：
 
@@ -22,13 +22,39 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func levelOrderBottom(root *TreeNode) [][]int { // faster 95.65% less 25%
+func levelOrderBottom(root *TreeNode) [][]int { // faster 51.06% less 100%
+	var temp [][]int
+	r(&temp, root, 0)
+	res := make([][]int, len(temp))
+	for k := range res {
+		res[k] = temp[len(temp)-1-k]
+	}
+	return res
+}
 
-
+func r(temp *[][]int, root *TreeNode, level int) {
+	if root == nil {
+		return
+	}
+	if len(*temp) <= level {
+		*temp = append(*temp, []int{})
+	}
+	(*temp)[level] = append((*temp)[level], root.Val)
+	r(temp, root.Left, level+1)
+	r(temp, root.Right, level+1)
 }
 
 func main() {
 	root := new(TreeNode)
+	root.Val = 3
+	root.Left = new(TreeNode)
+	root.Right = new(TreeNode)
+	root.Left.Val = 9
+	root.Right.Val = 20
+	root.Right.Left = new(TreeNode)
+	root.Right.Right = new(TreeNode)
+	root.Right.Left.Val = 15
+	root.Right.Right.Val = 7
 	result := levelOrderBottom(root)
 	for k := range result {
 		fmt.Println(result[k])
---
+++ 108.Convert Sorted Array to Binary Search Tree/solution.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+要求：
+
+
+解题思路：
+
+
+关键点：
+
+
+*/
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func sortedArrayToBST(nums []int) *TreeNode { // faster 51.06% less 100%
+
+}
+
+func main() {
+	nums := []int{}
+	result := sortedArrayToBST(nums)
+	fmt.Println(result)
+}
