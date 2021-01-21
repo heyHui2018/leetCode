0d56100f5aec61f5eb1cef5534052507a589c9e4
heyHui2018 <962691478@qq.com>
2021-01-21 15:08:55 +0800

    add 138

--- 138.Copy List with Random Pointer/solution.go
+++ 138.Copy List with Random Pointer/solution.go
@@ -9,7 +9,7 @@ import (
 
 
 解题思路：
-
+第一次遍历拷贝next，第二次遍历拷贝random
 
 关键点：
 
@@ -22,8 +22,24 @@ type Node struct {
 	Random *Node
 }
 
-func copyRandomList(head *Node) *Node { // faster 98.63% less 100%
-
+func copyRandomList(head *Node) *Node { // faster 100% less 25%
+	if head == nil {
+		return nil
+	}
+	h := head
+	nMap := make(map[*Node]*Node)
+	for h != nil {
+		temp := &Node{Val: h.Val}
+		nMap[h] = temp
+		h = h.Next
+	}
+	h = head
+	for h != nil {
+		nMap[h].Next = nMap[h.Next]
+		nMap[h].Random = nMap[h.Random]
+		h = h.Next
+	}
+	return nMap[head]
 }
 
 func main() {
---
+++ 139.Word Break/solution.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+要求：
+
+
+解题思路：
+
+
+关键点：
+
+
+*/
+
+func wordBreak(s string, wordDict []string) bool { // faster 100% less 25%
+
+}
+
+func main() {
+	s := ""
+	wordDict := []string{"1"}
+	result := wordBreak(s, wordDict)
+	fmt.Println(result)
+}
