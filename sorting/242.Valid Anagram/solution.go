f5e283968e26d0fd014b4268250d1beaf35b43b5
heyHui2018 <962691478@qq.com>
2021-11-29 17:38:27 +0800

    add 242

--- sorting/242.Valid Anagram/solution.go
+++ sorting/242.Valid Anagram/solution.go
@@ -9,15 +9,28 @@ import (
 
 
 解题思路：
-
+遍历s记数各字母，遍历t在s的记录上减
 
 关键点：
 
 
 */
 
-func isAnagram(s string, t string) bool { // faster 27.78% less 18.06%
-	return false
+func isAnagram(s string, t string) bool { // faster 28.88% less 19.52%
+	if len(s) != len(t) {
+		return false
+	}
+	m := make(map[string]int)
+	for _, v := range s {
+		m[string(v)]++
+	}
+	for _, v := range t {
+		m[string(v)]--
+		if m[string(v)] < 0 {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
---
+++ sorting/268.Missing Number/solution.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+要求：
+
+
+解题思路：
+
+
+关键点：
+
+
+*/
+
+func missingNumber(nums []int) int { // faster 28.88% less 19.52%
+
+	return 0
+}
+
+func main() {
+	fmt.Println(missingNumber([]int{1}))
+}
