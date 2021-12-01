f3ae5c847b0906a7266f7f149665cfcf03b73ae0
heyHui2018 <962691478@qq.com>
2021-12-01 18:00:46 +0800

    add 274

--- sorting/274.H-Index/solution.go
+++ sorting/274.H-Index/solution.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 /*
@@ -9,6 +10,7 @@ import (
 
 
 解题思路：
+排序，从前往后找 值 大于等于 剩余元素数 的序号，结果为剩余元素数
 
 
 关键点：
@@ -16,10 +18,16 @@ import (
 
 */
 
-func hIndex(citations []int) int { // faster 98.59% less 100%
+func hIndex(citations []int) int { // faster 100% less 100%
+	sort.Ints(citations)
+	for i := range citations {
+		if citations[i] >= len(citations)-i {
+			return len(citations) - i
+		}
+	}
 	return 0
 }
 
 func main() {
-	fmt.Println(hIndex([]int{3, 0, 1}))
+	fmt.Println(hIndex([]int{3, 0, 6, 1, 5}))
 }
---
+++ sorting/295.Find Median from Data Stream/solution.go
@@ -0,0 +1,39 @@
+package main
+
+/*
+要求：
+
+
+解题思路：
+排序，从前往后找 值 大于等于 剩余元素数 的序号，结果为剩余元素数
+
+
+关键点：
+
+
+*/
+
+type MedianFinder struct {
+}
+
+func Constructor() MedianFinder {
+
+}
+
+func (this *MedianFinder) AddNum(num int) {
+
+}
+
+func (this *MedianFinder) FindMedian() float64 { // faster 100% less 100%
+
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.AddNum(num);
+ * param_2 := obj.FindMedian();
+ */
+
+func main() {
+}
