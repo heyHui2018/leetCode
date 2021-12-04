7c0d297d5d52c24e09642e51e2e924c0050428ab
heyHui2018 <962691478@qq.com>
2021-12-04 19:09:55 +0800

    add 347

--- sorting/347.Top K Frequent Elements/solution.go
+++ sorting/347.Top K Frequent Elements/solution.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 /*
@@ -9,6 +10,7 @@ import (
 
 
 解题思路：
+map统计各数字出现频次，二维数组统计各频次有哪些数字，从大到小输出即可
 
 
 关键点：
@@ -16,11 +18,22 @@ import (
 
 */
 
-func topKFrequent(nums []int, k int) []int { // faster 70.83% less 33.33%
-	return nil
+func topKFrequent(nums []int, k int) []int { // faster 87.06% less 90.36%
+	m := make(map[int]int)
+	for i := range nums {
+		m[nums[i]]++
+	}
+	var f []int
+	for k := range m {
+		f = append(f, k)
+	}
+	sort.Slice(f, func(a, b int) bool {
+		return m[f[a]] > m[f[b]]
+	})
+	return f[:k]
 }
 
 func main() {
-	nums := []int{1, 5, 1, 1, 6, 4}
-	fmt.Println(topKFrequent(nums, 0))
+	nums := []int{4, 1, -1, 2, -1, 2, 3}
+	fmt.Println(topKFrequent(nums, 2))
 }
---
+++ sorting/349.Intersection of Two Arrays/solution.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+要求：
+
+
+解题思路：
+
+
+关键点：
+
+
+*/
+
+func intersection(nums1 []int, nums2 []int) []int { // faster 87.06% less 90.36%
+
+}
+
+func main() {
+	nums := []int{4, 1, -1, 2, -1, 2, 3}
+	fmt.Println(intersection(nums, nums))
+}
