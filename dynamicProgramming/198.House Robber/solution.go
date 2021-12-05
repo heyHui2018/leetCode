0f82ee7795d788eb26f37afa956e786b323ab4fc
heyHui2018 <962691478@qq.com>
2021-12-05 17:22:14 +0800

    add 349

---
+++ dynamicProgramming/198.House Robber/solution.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+要求：
+
+
+解题思路：
+
+
+关键点：
+
+
+*/
+
+func rob(nums []int) int { // faster 76.92% less 100%
+
+	return 0
+}
+
+func main() {
+	nums := []int{-1, -2}
+	result := rob(nums)
+	fmt.Println(result)
+}
--- sorting/349.Intersection of Two Arrays/solution.go
+++ sorting/349.Intersection of Two Arrays/solution.go
@@ -9,6 +9,7 @@ import (
 
 
 解题思路：
+遍历nums1存入map，遍历nums2与map比较得出交集
 
 
 关键点：
@@ -16,8 +17,23 @@ import (
 
 */
 
-func intersection(nums1 []int, nums2 []int) []int { // faster 87.06% less 90.36%
-
+func intersection(nums1 []int, nums2 []int) []int { // faster 100% less 51.69%
+	m := make(map[int]struct{})
+	for _, v := range nums1 {
+		m[v] = struct{}{}
+	}
+	var result []int
+	mm := make(map[int]struct{})
+	for _, v := range nums2 {
+		_, ok1 := m[v]
+		_, ok2 := mm[v]
+		if ok1 && !ok2 {
+			mm[v] = struct{}{}
+			result = append(result, v)
+			continue
+		}
+	}
+	return result
 }
 
 func main() {
