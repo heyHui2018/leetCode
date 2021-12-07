3acfb4656f971b385ec499399f214981125355bf
heyHui2018 <962691478@qq.com>
2021-12-07 20:33:35 +0800

    add 199

--- dynamicProgramming/199.Binary Tree Right Side View/solution.go
+++ dynamicProgramming/199.Binary Tree Right Side View/solution.go
@@ -9,7 +9,8 @@ import (
 
 
 解题思路：
-dp[i] = max(dp[i-2]+nums[i],dp[i-1])
+二叉树的右视图，定义a、b、c三个数组，a存放当前层的节点，b存放a中节点的子字节，c存放结果
+取b中最右节点的值放入c，随后将b复制进a，清空b
 
 
 关键点：
@@ -23,12 +24,54 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func rightSideView(root *TreeNode) []int { // faster 100% less 67.27%
+func rightSideView(root *TreeNode) []int { // faster 100% less 20.35%
+	if root == nil {
+		return []int{}
+	}
+	var a []*TreeNode
+	a = append(a, root)
+	result := []int{root.Val}
+	for len(a) > 0 {
+		var b []*TreeNode
+		for i := range a {
+			if a[i].Left != nil {
+				b = append(b, a[i].Left)
+			}
+			if a[i].Right != nil {
+				b = append(b, a[i].Right)
+			}
+		}
+		if len(b) == 0 {
+			return result
+		}
+		result = append(result, b[len(b)-1].Val)
 
-	return nil
+		a = make([]*TreeNode, len(b))
+		for i := range b {
+			a[i] = b[i]
+		}
+		b = []*TreeNode{}
+	}
+	return result
 }
 
 func main() {
-	result := rightSideView(nil)
+	// [1,2,3,null,5,null,4]
+	root := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+			Right: &TreeNode{
+				Val: 5,
+			},
+		},
+		Right: &TreeNode{
+			Val: 3,
+			Right: &TreeNode{
+				Val: 4,
+			},
+		},
+	}
+	result := rightSideView(root)
 	fmt.Println(result)
 }
---
+++ dynamicProgramming/200.Number of Islands/solution.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+要求：
+
+
+解题思路：
+
+
+关键点：
+
+
+*/
+
+func numIslands(grid [][]byte) int { // faster 100% less 20.35%
+
+	return 0
+}
+
+func main() {
+	result := numIslands([][]byte{})
+	fmt.Println(result)
+}
