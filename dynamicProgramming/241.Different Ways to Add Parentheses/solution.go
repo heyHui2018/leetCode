0898902d75afd9f225e13a02c253a4ecf459f90f
heyHui2018 <962691478@qq.com>
2021-12-13 20:55:08 +0800

    add 241

--- dynamicProgramming/241.Different Ways to Add Parentheses/solution.go
+++ dynamicProgramming/241.Different Ways to Add Parentheses/solution.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 /*
@@ -9,6 +10,7 @@ import (
 
 
 解题思路：
+抽象成()?()，分治法，递归，得到左右两边的结果后，遍历计算
 
 
 关键点：
@@ -16,12 +18,34 @@ import (
 
 */
 
-func diffWaysToCompute(expression string) []int { // faster 100% less 42.86%
-
-	return nil
+func diffWaysToCompute(expression string) []int { // faster 100% less 53.85%
+	i, err := strconv.Atoi(expression)
+	if err == nil {
+		return []int{i}
+	}
+	var result []int
+	for i := range expression {
+		if expression[i] == '+' || expression[i] == '-' || expression[i] == '*' {
+			left := diffWaysToCompute(expression[:i])
+			right := diffWaysToCompute(expression[i+1:])
+			for j := range left {
+				for k := range right {
+					switch expression[i] {
+					case '+':
+						result = append(result, left[j]+right[k])
+					case '-':
+						result = append(result, left[j]-right[k])
+					default:
+						result = append(result, left[j]*right[k])
+					}
+				}
+			}
+		}
+	}
+	return result
 }
 
 func main() {
-	result := diffWaysToCompute("")
+	result := diffWaysToCompute("2-1-1")
 	fmt.Println(result)
 }
---
+++ dynamicProgramming/264.Ugly Number II/solution.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+要求：
+
+
+解题思路：
+
+
+关键点：
+
+
+*/
+
+func nthUglyNumber(n int) int { // faster 100% less 53.85%
+
+	return 0
+}
+
+func main() {
+	result := nthUglyNumber(1)
+	fmt.Println(result)
+}
