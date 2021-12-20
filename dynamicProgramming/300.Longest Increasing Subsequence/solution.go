d8f2ae7d4f2310edea63cdddf17418497696ed09
heyHui2018 <962691478@qq.com>
2021-12-20 17:20:08 +0800

    add 300

--- dynamicProgramming/300.Longest Increasing Subsequence/solution.go
+++ dynamicProgramming/300.Longest Increasing Subsequence/solution.go
@@ -9,17 +9,35 @@ import (
 
 
 解题思路：
+dp[i]表示以nums[i]结尾的递增子序列的长度，dp中的最大值即为结果
+遍历0-i，当nums[i]>nums[j]&&dp[j]+1>dp[i]时，dp[i]=dp[j]+1，即将i更新成其之前最长自增序列的长度
 
 
 关键点：
 
 
 */
-func lengthOfLIS(nums []int) int { // faster 54.05% less 74.05%
-	return 0
+func lengthOfLIS(nums []int) int { // faster 64.47% less 34.12%
+	if len(nums) == 0 {
+		return 0
+	}
+	dp := make([]int, len(nums))
+	result := 0
+	for i := 0; i < len(nums); i++ {
+		dp[i] = 1
+		for j := 0; j < i; j++ {
+			if nums[i] > nums[j] && dp[j]+1 > dp[i] {
+				dp[i] = dp[j] + 1
+			}
+		}
+		if dp[i] > result {
+			result = dp[i]
+		}
+	}
+	return result
 }
 
 func main() {
-	result := lengthOfLIS([]int{})
+	result := lengthOfLIS([]int{10, 9, 2, 5, 3, 7, 101, 18, 1})
 	fmt.Println(result)
 }
---
+++ dynamicProgramming/309.Best Time to Buy and Sell Stock with Cooldown/solution.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+要求：
+
+
+解题思路：
+
+
+关键点：
+
+
+*/
+func maxProfit(prices []int) int { // faster 64.47% less 34.12%
+	return 0
+}
+
+func main() {
+	result := maxProfit([]int{10, 9, 2, 5, 3, 7, 101, 18, 1})
+	fmt.Println(result)
+}
