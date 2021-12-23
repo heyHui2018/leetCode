cd2c3a2085de708e669629be29c1e4f7ec690009
heyHui2018 <962691478@qq.com>
2021-12-23 21:24:47 +0800

    add 309

--- dynamicProgramming/309.Best Time to Buy and Sell Stock with Cooldown/solution.go
+++ dynamicProgramming/309.Best Time to Buy and Sell Stock with Cooldown/solution.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 /*
@@ -9,14 +10,33 @@ import (
 
 
 解题思路：
+持有，售出，休息
+售出的收益等于持有加上当天价格
+持有的收益等于max(前一天休息之后买入，前一天持有）
+休息的收益等于max(前一天休息，前一天售出)
 
 
 关键点：
 
 
 */
-func maxProfit(prices []int) int { // faster 64.47% less 34.12%
-	return 0
+
+func maxProfit(prices []int) int { // faster 100% less 100%
+	sold, rest, hold := 0, 0, math.MinInt32
+	for _, v := range prices {
+		preSold := sold
+		sold = hold + v
+		hold = max(hold, rest-v)
+		rest = max(rest, preSold)
+	}
+	return max(rest, sold)
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
 }
 
 func main() {
---
+++ dynamicProgramming/312.Burst Balloons/solution.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+要求：
+
+
+解题思路：
+
+
+关键点：
+
+
+*/
+
+func maxCoins(nums []int) int { // faster 100% less 100%
+	return 0
+}
+
+func main() {
+	result := maxCoins([]int{10, 9, 2, 5, 3, 7, 101, 18, 1})
+	fmt.Println(result)
+}
